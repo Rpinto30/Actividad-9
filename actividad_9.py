6232bffd4e8534a25c4e78bc6fe0bc3d74bc3e65
Rpinto30 <rodrigopinto3007@gmail.com>
2025-07-26 15:21:55 -0600

    Creacion de una funcion qeu permita ingresar enteros

--- actividad_9.py
+++ actividad_9.py
@@ -1,7 +1,33 @@
 movies = []
+error_mesagge = '\n'+"✖"*5+"   Lo siento, intentelo nuevamente   "+"✖"*5+'\n'
+
+def input_integer(message): #INGRESAR UN ENTERO Y VERIFICAR QUE SU ENTRADA SEA VALIDA
+    while True:
+        try:
+            value = int(input(message))
+            break
+        except ValueError: print(error_mesagge)
+    return value
+
+def set_movie():
+
+    movies_size = input_integer("▶Ingresa la cantidad de peliculas que deseas ingresar: ")
+
+    for mov in range(movies_size):
+        movie = []
+        print("-"*15+f"PELÍCULA {mov}"+"-"*15)
+        movie_name = int(input("▶Ingresa el nombre de la película: "))
+        while True:
+            try:
+                movies_size = int(input("▶Ingresa la cantidad de peliculas que deseas ingresar: "))
+                break
+            except ValueError:
+                print(error_mesagge)
+
+
 
 while True:
-    print("-"*20+"Metflix"+"-"*20)
+    print("-"*20+"METFLIX"+"-"*20)
     print(" 1)Agregar pelicula\n 2)Mostrar todas las peliculas\n 3)Buscar pelicula por género\n 4)Eliminar una pelicula por titulo\n 5)Ver estradísticas del cátalogo\n 6)Salir")
     op = input('Ingrese una de las opciones: ')
     match op:
@@ -13,4 +39,4 @@ while True:
         case '6':
             print("\n ▢  ¡¡¡Hasta luego!!!")
             break
-        case _: print('\n'+"✖"*5+"   Lo siento, intentelo nuevamente   "+"✖"*5+'\n')
+        case _: print(error_mesagge)
