edece5a756adab4843a4ddf7f7a5a826308adc8c
Rpinto30 <rodrigopinto3007@gmail.com>
2025-07-26 15:29:34 -0600

    Creacion de una funcion que permite agregar peliculas a la lista

--- actividad_9.py
+++ actividad_9.py
@@ -1,4 +1,6 @@
 movies = []
+
+genres = ['Acción', 'Drama', 'Comedia', 'Terror']
 error_mesagge = '\n'+"✖"*5+"   Lo siento, intentelo nuevamente   "+"✖"*5+'\n'
 
 def input_integer(message): #INGRESAR UN ENTERO Y VERIFICAR QUE SU ENTRADA SEA VALIDA
@@ -16,13 +18,13 @@ def set_movie():
     for mov in range(movies_size):
         movie = []
         print("-"*15+f"PELÍCULA {mov}"+"-"*15)
-        movie_name = int(input("▶Ingresa el nombre de la película: "))
-        while True:
-            try:
-                movies_size = int(input("▶Ingresa la cantidad de peliculas que deseas ingresar: "))
-                break
-            except ValueError:
-                print(error_mesagge)
+        movie_name = input("▶Ingresa el nombre de la película: ")
+        movie_year = input_integer("▶Ingresa el año de la película: ")
+        for i,j in enumerate(genres): print(f"  {i+1}) {j}")
+        movie_genre = input_integer("▶Ingresa el genero de la película: ")
+
+        movies.append([movie_name, movie_year, movie_genre])
+        print(f"\n PELÍCULA: {movie_name} del año {movie_year}, {movie_genre} agregada correctamente")
 
 
 
@@ -31,12 +33,13 @@ while True:
     print(" 1)Agregar pelicula\n 2)Mostrar todas las peliculas\n 3)Buscar pelicula por género\n 4)Eliminar una pelicula por titulo\n 5)Ver estradísticas del cátalogo\n 6)Salir")
     op = input('Ingrese una de las opciones: ')
     match op:
-        case '1': pass
+        case '1': set_movie()
         case '2': pass
         case '3': pass
         case '4': pass
         case '5': pass
         case '6':
+            print(movies)
             print("\n ▢  ¡¡¡Hasta luego!!!")
             break
         case _: print(error_mesagge)
